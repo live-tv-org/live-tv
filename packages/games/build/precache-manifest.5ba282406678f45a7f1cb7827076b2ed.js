6734e783936696f00a32fa3445b213cb9ac0f61d
alex <gaer87@gmail.com>
2019-11-04 15:31:28 +0300

    feat: add build

---
+++ packages/games/build/precache-manifest.5ba282406678f45a7f1cb7827076b2ed.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da17feee66c55cbb0a6eb6620c101960",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be28a642b1c8dc5edf64",
+    "url": "/static/css/main.b0bb7f2c.css"
+  },
+  {
+    "revision": "be28a642b1c8dc5edf64",
+    "url": "/static/js/main.d61005c8.js"
+  }
+]);
