0bd3820e0dd8c7985a44a7be0169e8b85946c1fb
alex <gaer87@gmail.com>
2019-11-04 15:51:20 +0300

    serve

--- packages/container/build/service-worker.js
+++ packages/container/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dc79e48e7036425888afabcc7e720bd2.js"
+  "https://gaer87.github.io/live-tv/packages/container/build/precache-manifest.1b430fc3519edc74b44300ec72ac9113.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://gaer87.github.io/live-tv/packages/container/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
