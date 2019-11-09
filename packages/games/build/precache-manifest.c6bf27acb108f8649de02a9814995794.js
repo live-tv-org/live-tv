0c8688a2b1b253aeaf547ce036d5406d638ba6ad
alex <gaer87@gmail.com>
2019-11-09 22:21:10 +0300

    feat(games): build

---
+++ packages/games/build/precache-manifest.c6bf27acb108f8649de02a9814995794.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec1fc50123fb07b60e13f123173d7267",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4b4f0806f069324ac1f",
+    "url": "/static/css/main.b0bb7f2c.css"
+  },
+  {
+    "revision": "f4b4f0806f069324ac1f",
+    "url": "/static/js/main.0a694010.js"
+  }
+]);
--- packages/games/build/service-worker.js
+++ packages/games/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5ba282406678f45a7f1cb7827076b2ed.js"
+  "/precache-manifest.c6bf27acb108f8649de02a9814995794.js"
 );
 
 self.addEventListener('message', (event) => {
