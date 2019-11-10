4bfdf32e64b916a4f603420762c9c7e70976fcb2
alex <gaer87@gmail.com>
2019-11-10 15:52:42 +0300

    Build

---
+++ packages/container/build/precache-manifest.f10ee788c3c8321767c3dbef4d7a10fe.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8c12aa3c82088b0c1c6d149fe42c23",
+    "url": "https://gaer87.github.io/live-tv/packages/container/build/index.html"
+  },
+  {
+    "revision": "472d5c5da8585b0840c8",
+    "url": "https://gaer87.github.io/live-tv/packages/container/build/static/js/2.ad2e9331.chunk.js"
+  },
+  {
+    "revision": "0594443c4953964a68fd",
+    "url": "https://gaer87.github.io/live-tv/packages/container/build/static/js/main.24ef3d9f.chunk.js"
+  },
+  {
+    "revision": "32a84f6921d7b8eee539",
+    "url": "https://gaer87.github.io/live-tv/packages/container/build/static/js/runtime~main.4d27a303.js"
+  }
+]);
