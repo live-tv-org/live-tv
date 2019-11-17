adc0afa8d49a531751c85c8a69c397f51bb6a83b
alex <gaer87@gmail.com>
2019-11-17 14:18:09 +0300

    New build

---
+++ docs/precache-manifest.8a02a499e8510efbf391785e03eb866e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ddc091b8bdb75a28e95cc6da92319cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "472d5c5da8585b0840c8",
+    "url": "/static/js/2.ad2e9331.chunk.js"
+  },
+  {
+    "revision": "9d64ca003605b3fdad52",
+    "url": "/static/js/main.321c4cc8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://gaer87.github.io/live-tv/packages/container/build/precache-manifest.f10ee788c3c8321767c3dbef4d7a10fe.js"
+  "/precache-manifest.8a02a499e8510efbf391785e03eb866e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://gaer87.github.io/live-tv/packages/container/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
