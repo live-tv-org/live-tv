902f54cec2d333b57527466ab2ee0c87c1527c01
alex <gaer87@gmail.com>
2019-11-25 20:50:47 +0300

    style(games): remove the unused dependency

--- packages/games/src/components/Streams.js
+++ packages/games/src/components/Streams.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { action } from 'mobx'
 import { observer } from 'mobx-react'
 import styled  from 'styled-components'
 import Button  from 'react-bootstrap/Button'
